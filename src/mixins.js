e0ee06410151869e14bc988a0f4e6ff804f7b2c2
hm4725 <4725hmc@gmail.com>
2021-11-25 21:20:35 +0900

    add cross origin cookie axios setting

--- src/mixins.js
+++ src/mixins.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const HOST = "http://lasqua.bludi-blu.com"
-
+const HOST = "https://lasqua.bludi-blu.com"
+axios.defaults.withCredentials = true
 export default {
     methods: {
         async $api(path, method, data) {
