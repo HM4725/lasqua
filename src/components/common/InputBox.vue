0a1965db51e0ab26cc2b8082c39607ce728a8ce0
hm4725 <4725hmc@gmail.com>
2021-11-30 15:43:11 +0000

    add signup page

---
+++ src/components/common/InputBox.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="input-box" :class="{filled: filled}">
+    <input :id="title" :placeholder="title" :type="type" v-model="value"/>
+    <label :for="title">{{title}}</label>
+  </div>
+</template>
+
+<script>
+export default{
+  props: {
+    type: {
+      type: String,
+      default: "text"
+    },
+    title: {
+      type: String,
+      default: "Box"
+    }
+  },
+  data() {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    getValue() {
+      return this.value
+    }
+  },
+  computed: {
+    filled() {
+      return this.value.length !== 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .input-box {
+    position: relative;
+  }
+  .input-box input {
+    transition: all 1s;
+    margin-top: .5rem;
+    padding: 4px 6px;
+    border: 1px solid var(--base-color);
+    border-radius: 0;
+    background-color: white;
+  }
+  .input-box input::placeholder {
+    transition: opacity .25s ease-out;
+    opacity: 1;
+  }
+  .input-box label {
+    position: absolute;
+    left: 7px;
+    top: 1rem;
+    padding: 0 1px;
+    font-size: .9rem;
+    height: 1rem;
+    line-height: 1rem;
+    transition: all 0.5s ease-out;
+    overflow: hidden;
+    white-space: nowrap;
+    z-index: 1;
+    opacity: 0;
+  }
+  .input-box input:focus::placeholder {
+    opacity: 0;
+  }
+  .input-box input:focus + label,
+  .input-box.filled input + label {
+    opacity: 1;
+    top: .1rem;
+    background-color: white;
+  }
+  .input-box input:focus{
+    outline: none;
+    border-color: var(--active-color);
+  }
+</style>
--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    
+    <form>
+      <input-box title="아이디" type="text"/>
+      <input-box title="비밀번호" type="password"/>
+      <input-box title="이름" type="text"/>
+      <input-box title="이메일" type="email"/>
+      <input-box title="전화번호" type="tel"/>
+      <span>Optional</span>
+      <input-box title="생년월일" type="date"/>
+      <input-box title="자기소개" type="text"/>
+      <input-box title="페이스북" type="text"/>
+      <input-box title="인스타그램" type="text"/>
+      <input-box title="트위터" type="text"/>
+      <input-box title="기타SNSID" type="text"/>
+      <input-box title="기타SNSLink" type="text"/>
+    </form>
   </div>
 </template>
 
 <script>
+import InputBox from '@/components/common/InputBox.vue'
+
 export default{
-  
+  components: {
+    InputBox
+  }
 }
 </script>
 
