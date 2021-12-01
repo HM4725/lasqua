eca408c13fa5f8e77c37d5d5c12e5eeeb2f999cd
hm4725 <4725hmc@gmail.com>
2021-12-01 09:27:54 +0000

    signup

--- src/views/user/SignupPage.vue
+++ src/views/user/SignupPage.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
     <form class="signup">
-      <div class="necessary">
-        <p class="f-title">필수 사항</p>
-        <input-box id="id" type="text" placeholder="아이디"/>
-        <input-box id="pw" type="password" placeholder="비밀번호"/>
-        <input-box id="pw" type="password" placeholder="비밀번호 확인"/>
-        <input-box id="name" type="text" placeholder="이름"/>
-        <input-box id="email" type="email" placeholder="이메일"/>
-        <input-box id="phone" type="tel" placeholder="전화번호" />
+      <div class="signup-info">
+        <div class="necessary">
+          <p class="f-title">필수 사항</p>
+          <input-box id="signup-id" type="text" placeholder="아이디" ref="id"/>
+          <input-box id="signup-pw" type="password" placeholder="비밀번호" ref="pw"/>
+          <input-box id="signup-confirmPw" type="password" placeholder="비밀번호 확인" ref="confirmPw"/>
+          <input-box id="signup-name" type="text" placeholder="이름" ref="name"/>
+          <input-box id="signup-email" type="email" placeholder="이메일" ref="email"/>
+          <input-box id="signup-phone" type="tel" placeholder="전화번호" ref="phone"/>
+        </div>
+        <div class="optional">
+          <p class="f-title">선택 사항</p>
+          <radio-box id="signup-company" title="회원 구분" :options="company.options" :checked="company.checked" ref="company"/>
+          <radio-box id="signup-gender" title="성별" :options="gender.options" :checked="gender.checked" ref="gender"/>
+          <input-box id="signup-regdate" type="date" placeholder="생년월일" ref="birth"/>
+          <input-box id="signup-info" type="text" placeholder="간단한 소개" ref="info"/>
+          <input-box id="signup-facebook" type="text" placeholder="페이스북 ID" ref="facebook"/>
+          <input-box id="signup-instagram" type="text" placeholder="인스타그램 ID" ref="instagram"/>
+          <input-box id="signup-twitter" type="text" placeholder="트위터 ID" ref="twitter"/>
+          <input-box id="signup-etc_name" type="text" placeholder="기타 SNS ID" ref="etcName"/>
+          <input-box id="signup-etc_link" type="text" placeholder="기타 SNS 링크" ref="etcLink"/>
+        </div>
       </div>
-      <div class="optional">
-        <p class="f-title">선택 사항</p>
-        <radio-box id="sex" title="성별" :options="sex.options" :checked="sex.checked"/>
-        <radio-box id="company" title="구분" :options="company.options" :checked="company.checked"/>
-        <input-box id="birth" type="date" placeholder="생년월일"/>
-        <input-box id="info" type="text" placeholder="간단한 소개"/>
-        <input-box id="facebook" type="text" placeholder="페이스북 ID"/>
-        <input-box id="instagram" type="text" placeholder="인스타그램 ID"/>
-        <input-box id="twitter" type="text" placeholder="트위터 ID"/>
-        <input-box id="etc_name" type="text" placeholder="기타 SNS ID"/>
-        <input-box id="etc_link" type="text" placeholder="기타 SNS 링크"/>
+      <div>
+        <default-button @click="signup" value="회원가입"/>
       </div>
     </form>
   </div>
@@ -29,23 +34,62 @@
 <script>
 import InputBox from '@/components/form/InputBox.vue'
 import RadioBox from '@/components/form/RadioBox.vue'
+import DefaultButton from '@/components/buttons/DefaultButton.vue'
 
 export default{
   components: {
     InputBox,
-    RadioBox
+    RadioBox,
+    DefaultButton
   },
   data() {
     return {
-      sex: {
+      gender: {
         options: {M: '남자', F: '여자', N: '그 외'},
         checked: 'N'
       },
       company: {
         options: {Y: '기업회원', N: '일반회원'},
         checked: 'N'
-      }
+      },
+      error: false
     }
+  },
+  methods: {
+    async signup(e) {
+      e.preventDefault()
+      this.error = false;
+      const newUser = {
+        id: this.$refs.id.getValue(), 
+        pw: this.$refs.pw.getValue(),
+        name: this.$refs.name.getValue(),
+        email: this.$refs.email.getValue(),
+        phone: this.$refs.phone.getValue(),
+        company: this.$refs.company.getValue(),
+        gender: this.$refs.gender.getValue(),
+        birth: this.$refs.birth.getValue(),
+        info: this.$refs.info.getValue(),
+        facebook: this.$refs.facebook.getValue(),
+        instagram: this.$refs.instagram.getValue(),
+        twitter: this.$refs.twitter.getValue(),
+        etc_name: this.$refs.etcName.getValue(),
+        etc_link: this.$refs.etcLink.getValue()
+      }
+      const response = await this.$api("POST", "/signup", newUser)
+      console.log(response)
+      // if(result === "success") {
+      //   const redirect = this.$route.query.redirect
+      //   if(redirect) {
+      //     this.$router.push(redirect)
+      //   } else {
+      //     this.$router.go(-1)
+      //   }
+      // } else {
+      //   this.$refs.id.occurError();
+      //   this.$refs.pw.occurError();
+      //   this.error = true
+      // }
+    },
   }
 }
 </script>
@@ -53,12 +97,16 @@ export default{
 <style scoped>
   .signup {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
   }
-  .signup > div {
+  .signup-info {
+    display: flex;
+    flex-direction :row;
+  }
+  .signup-info > div {
     margin: 1rem;
   }
-  .signup > div > * {
+  .signup-info > div > * {
     margin: .5rem 0;
   }
 </style>
