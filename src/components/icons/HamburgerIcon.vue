0a2f52239269ee4ca026049651a8cf4d391a4008
hm4725 <4725hmc@gmail.com>
2021-12-09 13:51:24 +0000

    remove redundant attr

--- src/components/icons/HamburgerIcon.vue
+++ src/components/icons/HamburgerIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <base-icon :size="size" viewBox="0 0 24 24">
-    <line x1="3" y1="12" x2="21" y2="12" stroke-linecap="round"/>
+    <line x1="3" y1="12" x2="21" y2="12"/>
     <line x1="3" y1="6" x2="21" y2="6"/>
     <line x1="3" y1="18" x2="21" y2="18"/>
   </base-icon>
