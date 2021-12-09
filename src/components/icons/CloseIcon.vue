bddbe404b9f8b7aa6a673204d2147f443277ad47
hm4725 <4725hmc@gmail.com>
2021-12-09 13:52:00 +0000

    add close icon

---
+++ src/components/icons/CloseIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-icon class="close-icon" :size="size" viewBox="0 0 24 24">
+    <line x1="6" y1="6" x2="18" y2="18"/>
+    <line x1="6" y1="18" x2="18" y2="6"/>
+  </base-icon>
+</template>
+
+<script>
+import BaseIcon from './BaseIcon.vue'
+
+export default{
+  components: {
+    BaseIcon
+  },
+  props: {
+    size: {
+     type: Number,
+      default: 24
+    }
+  }
+}
+</script>
+
+<style scoped>
+  line {
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-width: 2;
+  }
+</style>
