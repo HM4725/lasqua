e9bd4fb90a8a96e241d8c9476dec71f64c74bc50
hm4725 <4725hmc@gmail.com>
2021-12-18 07:22:41 +0000

    artists -> image profile

--- src/views/article/TravelerPage.vue
+++ src/views/article/TravelerPage.vue
@@ -9,6 +9,30 @@
 
 <script>
 import ArticleList from '@/components/article/ArticleList.vue'
+
+function getArticlelistFrom(userlist) {
+  const users = []
+  for(let i in userlist.users) {
+    let user = userlist.users[i]
+    users.push({
+      no: 0,
+      id: user.id,
+      title: user.id,
+      images: {
+        orderNo: 0,
+        name: user.id,
+        link: user.image || require('@/assets/img/default-profile.png')
+      }
+    })
+  }
+  return {
+    allArticleCount: userlist.allUserCount,
+    page: userlist.page,
+    maxPage: userlist.maxPage,
+    articles: users
+  }
+}
+
 export default{
   components: {
     ArticleList
@@ -21,8 +45,8 @@ export default{
   methods: {
     async loadArtists(page) {
       try {
-        const response = await this.$api("GET", `/articlelist?page=${page}`)
-        const data = response.data
+        const response = await this.$api("GET", `/userlist?page=${page}`)
+        const data = getArticlelistFrom(response.data)
         if(page === data.page) {
           if(!this.init) {
             this.$refs.artists.init(data)
