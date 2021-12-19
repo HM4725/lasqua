fc443664f1112af3cc1b118f7a64cfc78b26ce9a
hm4725 <4725hmc@gmail.com>
2021-12-19 16:38:29 +0000

    search page finish

--- src/views/article/SearchPage.vue
+++ src/views/article/SearchPage.vue
@@ -2,15 +2,42 @@
   <div class="search-page">
     <div class="projects">
       <p class="title f-title">Projects</p>
+      <p v-if="notfound.articles">검색 결과가 없습니다.</p>
       <article-list rowlength="4" ref="articles" mode="project" @request-push="loadArticles"/>
     </div>
     <div class="artists">
+      <p class="title f-title">Artist</p>
+      <p v-if="notfound.artists">검색 결과가 없습니다.</p>
+      <article-list rowlength="4" ref="artists" mode="artist" @request-push="loadArtists"/>
     </div>
   </div>
 </template>
 
 <script>
 import ArticleList from '@/components/article/ArticleList.vue'
+
+function getArticlelistFrom(userlist) {
+  const users = []
+  for(let i in userlist.userList) {
+    let user = userlist.userList[i]
+    users.push({
+      no: 0,
+      id: user.id,
+      title: user.id,
+      images: {
+        orderNo: 0,
+        name: user.id,
+        link: user.profileImage || require('@/assets/img/default-profile.png')
+      }
+    })
+  }
+  return {
+    allArticleCount: userlist.allUserCount,
+    page: userlist.page,
+    maxPage: userlist.maxPage,
+    articles: users
+  }
+}
 
 export default{
   name: "WatcherPage",
@@ -20,22 +47,45 @@ export default{
   data() {
     return {
       keyword: '',
-      init: false
+      init: {
+        articles: false,
+        artists: false
+      },
+      notfound: {
+        articles: false,
+        artists: false
+      }
     }
   },
   methods: {
     async loadArticles(page) {
       try {
         const response = await this.$api("GET", `/search/articles?key=${this.keyword}&page=${page}`)
+        this.notfound.articles = false
         const data = response.data
-        if(!this.init) {
+        if(!this.init.articles) {
           this.$refs.articles.init(data)
-          this.init = true
+          this.init.articles = true
         } else {
           this.$refs.articles.push(data.articles)
         }
       } catch(error) {
-        console.error(error)
+        error.response.status === 404 && (this.notfound.articles = true)
+      }
+    },
+    async loadArtists(page) {
+      try {
+        const response = await this.$api("GET", `/search/users?key=${this.keyword}&page=${page}`)
+        this.notfound.artists = false
+        const data = getArticlelistFrom(response.data)
+        if(!this.init.artists) {
+          this.$refs.artists.init(data)
+          this.init.artists = true
+        } else {
+          this.$refs.artists.push(data.articles)
+        }
+      } catch(error) {
+        error.response.status === 404 && (this.notfound.artists = true)
       }
     }
   },
@@ -43,8 +93,12 @@ export default{
     '$route.query': {
       handler(query) {
         this.keyword = query.keyword
-        this.init = false
-        this.loadArticles(1)
+        if(this.keyword) {
+          this.init.articles = false
+          this.loadArticles(1)
+          this.init.artists = false
+          this.loadArtists(1)
+        }
       }
     }
   },
