deeea89ed8eeb33706ea66af58fd88244b56b3ad
hm4725 <4725hmc@gmail.com>
2021-12-30 08:35:27 +0000

    router: same component & different path -> reload

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <main class="main-wrapper">
     <router-view v-slot="{Component}">
       <transition name="fade" mode="out-in">
-        <component :is="Component"></component>
+        <component :key="$route.fullPath" :is="Component"></component>
       </transition>
     </router-view>
   </main>
