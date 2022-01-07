013fe3c010cb6b6d8c6cd1cf3eccc8daa71e45f5
hm4725 <4725hmc@gmail.com>
2022-01-07 18:18:14 +0000

    refactor articlelist -> search page

--- src/views/article/SearchPage.vue
+++ src/views/article/SearchPage.vue
@@ -2,19 +2,21 @@
   <div class="search-page">
     <div class="projects">
       <p class="title f-title">Projects</p>
-      <p v-if="notfound.articles">검색 결과가 없습니다.</p>
-      <article-list rowlength="4" ref="articles" mode="project" @request-push="loadArticles"/>
+      <p v-if="notfound.projects">검색 결과가 없습니다.</p>
+      <article-list ref="projects" :rowlength="4" paging="button"
+        @request="handleRequestProject" @clicked="handleClickProject"/>
     </div>
     <div class="artists">
       <p class="title f-title">Artist</p>
       <p v-if="notfound.artists">검색 결과가 없습니다.</p>
-      <article-list rowlength="4" ref="artists" mode="artist" @request-push="loadArtists"/>
+      <article-list ref="artists" :rowlength="4" paging="button"
+        @request="handleRequestArtist" @clicked="handleClickArtist"/>
     </div>
   </div>
 </template>
 
 <script>
-import ArticleList from '@/components/article/articlelist/ArticleList.vue'
+import ArticleList from '@/components/article/articlelist/MainList.vue'
 
 function getArticlelistFrom(userlist) {
   const users = []
@@ -47,46 +49,40 @@ export default{
   data() {
     return {
       keyword: '',
-      init: {
-        articles: false,
-        artists: false
-      },
       notfound: {
-        articles: false,
+        projects: false,
         artists: false
       }
     }
   },
   methods: {
-    async loadArticles(page) {
+    async handleRequestProject(payload) {
       try {
-        const response = await this.$api("GET", `/search/articles?key=${this.keyword}&page=${page}`)
-        this.notfound.articles = false
+        const response = await this.$api("GET", 
+          `/search/articles?key=${this.keyword}&page=${payload.page}`)
         const data = response.data
-        if(!this.init.articles) {
-          this.$refs.articles.init(data)
-          this.init.articles = true
-        } else {
-          this.$refs.articles.push(data.articles)
-        }
+        this.$refs.projects.inject(data, payload.way)
       } catch(error) {
-        error.response.status === 404 && (this.notfound.articles = true)
+        this.$refs.projects.inject({articles: []})
+        error.response.status === 404 && (this.notfound.projects = true)
       }
     },
-    async loadArtists(page) {
+    handleClickProject(no) {
+      this.$router.push(`/article/view?no=${no}`)
+    },
+    async handleRequestArtist(payload) {
       try {
-        const response = await this.$api("GET", `/search/users?key=${this.keyword}&page=${page}`)
-        this.notfound.artists = false
+        const response = await this.$api("GET", 
+          `/search/users?key=${this.keyword}&page=${payload.page}`)
         const data = getArticlelistFrom(response.data)
-        if(!this.init.artists) {
-          this.$refs.artists.init(data)
-          this.init.artists = true
-        } else {
-          this.$refs.artists.push(data.articles)
-        }
+        this.$refs.artists.inject(data, payload.way)
       } catch(error) {
+        this.$refs.artists.inject({articles: []})
         error.response.status === 404 && (this.notfound.artists = true)
       }
+    },
+    handleClickArtist(id) {
+      this.$router.push(`/artist/${id}`)
     }
   },
   created() {
