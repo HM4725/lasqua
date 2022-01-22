d94fea48e4548e47a4de4722f69a598424114100
hm4725 <4725hmc@gmail.com>
2022-01-22 12:01:02 +0000

    finish test

--- src/views/utils/TestPage.vue
+++ src/views/utils/TestPage.vue
@@ -6,21 +6,7 @@
 
 <script>
 export default{
-  name: 'utils.test.page',
-  methods: {
-    async test() {
-      try {
-        const id = this.$store.getters.userId
-        const response = await this.$api("GET", `/user/${id}/info`)
-        console.log(response.data)
-      } catch(error) {
-        console.error(error)
-      }
-    },
-  },
-  mounted() {
-    this.test() 
-  }
+  name: 'utils.test.page'
 }
 </script>
 
