cd78e555bf08d8c94c142148e16513739809ed56
hm4725 <4725hmc@gmail.com>
2022-02-02 15:40:30 +0000

    fix error: webpack-bundle-anlyzer stops jenkins

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,9 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').
-    BundleAnalyzerPlugin
-
 module.exports = {
     devServer: {
         disableHostCheck: true
     },
     productionSourceMap: false,
     configureWebpack: {
-    	mode: 'production',
-        plugins: [new BundleAnalyzerPlugin()]
+    	mode: 'production'
     }
 }
