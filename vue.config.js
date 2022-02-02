1b82ba36ff2b910ea272d1f99737aee71fbfa301
hm4725 <4725hmc@gmail.com>
2022-02-02 15:24:29 +0000

    Analysis vue bundles size

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').
+    BundleAnalyzerPlugin
+
 module.exports = {
     devServer: {
         disableHostCheck: true
+    },
+    configureWebpack: {
+        plugins: [new BundleAnalyzerPlugin()]
     }
 }
