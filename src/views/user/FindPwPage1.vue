b1ea699db46722962584a7c451ac095b393d0f18
hm4725 <4725hmc@gmail.com>
2022-02-11 16:38:58 +0000

    add findpw page

--- src/views/user/FindPwPage1.vue
+++ src/views/user/FindPwPage1.vue
@@ -1,15 +1,169 @@
 <template>
-  <div>
-    
-  </div>
+  <form class="findpw-page" @submit.prevent="submit" method="POST">
+    <h1>비밀번호 찾기</h1>
+    <p class="message">아이디와 이메일을 입력하세요.</p>
+    <div class="id-box">
+      <input-box id="findpw-id" type="text" placeholder="아이디"
+        @input="v=>{id.val=v}" :disabled="email.check" autocomplete="off"/>
+    </div>
+    <div class="email-box">
+      <input-box id="findpw-email" type="text" placeholder="이메일"
+        @input="v=>{email.val=v}" :disabled="email.check"/>
+    </div>
+    <div class="auth-box" v-show="email.check">
+      <input-box id="findpw-email-auth" type="text" placeholder="인증번호"
+        @input="v=>{email.number=v}" :disabled="email.auth"/>
+    </div>
+    <p class="error">{{msg}}</p>
+    <footer>
+      <default-button class="button" value="뒤로" @click="cancel"/>
+      <default-button class="button" type="submit" :value="email.button"/>
+    </footer>
+  </form>
 </template>
 
 <script>
+import DefaultButton from '@/components/buttons/DefaultButton.vue'
+import InputBox from '@/components/form/InputBox.vue'
+
 export default{
-  
+  name: 'user.findpw.page1',
+  components: {
+    DefaultButton,
+    InputBox
+  },
+  props: {
+    page: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      id: {
+        val: ''
+      },
+      email: {
+        val: '',
+        pattern: '^[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\\.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$',
+        constraint: '올바른 이메일을 입력하세요.',
+        check: false,
+        auth: false,
+        number: '',
+        button: '인증'
+      },
+      msg: ''
+    }
+  },
+  methods: {
+    async submit() {
+      if(this.id.val.length !== 0) {
+        if(this.email.val.length !== 0) {
+          if(this._isValid(this.email)) {
+            if(!this.email.check) {
+              if(await this._checkIdMail()) {
+                this.email.check = true
+                if(!await this._sendCheckMail()) {  
+                  this.email.check = false
+                }
+              }
+            } else {
+              if (await this._checkMailNumber()) {
+                const payload = {
+                  auth: this.email.number,
+                  email: this.email.val
+                }
+                this.$router.push({name: 'user.findpw2', params: payload})
+              } else {
+                this.msg = '인증 번호가 일치하지 않습니다.'
+              }
+            }
+          } else {
+            this.msg = this.email.constraint
+          }
+        } else {
+          this.msg = '이메일을 입력하세요.'
+        }
+      } else {
+        this.msg = '아이디를 입력하세요.'
+      }
+    },
+    cancel() {
+      this.$router.push('/login')
+    },
+    // Private
+    async _checkIdMail() {
+      try {
+        const payload = {id: this.id.val, email: this.email.val}
+        await this.$api('POST', '/mailcheck', payload)
+        return true
+      } catch(error) {
+        this.msg = '아이디와 이메일이 일치하지 않습니다.'
+        return false
+      }
+    },
+    async _sendCheckMail() {
+      try {
+        this.msg = '해당 메일로 인증번호를 보냈습니다.'
+        await this.$api('POST', '/mail?val=pw', {email: this.email.val})
+        this.msg = '메일에서 인증번호를 확인해 주세요.'
+        return true
+      } catch(error) {
+        this.msg = '인증 메일 발송에 오류가 발생하였습니다.'
+        return false
+      }
+    },
+    async _checkMailNumber() {
+      try {
+        const payload = {
+          email: this.email.val,
+          number: this.email.number
+        }
+        this.email.auth = true
+        await this.$api('POST', '/mailnumber', payload)
+        return true
+      } catch(error) {
+        this.msg = '인증 번호가 일치하지 않습니다.'
+        this.email.auth = false
+        return false
+      }
+    },
+    _isValid(target) {
+      return new RegExp(target.pattern).test(target.val)
+    },
+  }
 }
 </script>
 
 <style scoped>
-
+  .findpw-page {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    width: 300px;
+  }
+  .findpw-page > .message {
+    text-align: left;
+    padding-left: 8px;
+    margin-bottom: 16px;
+  }
+  .findpw-page > .email-box {
+    margin-top: 16px;
+  }
+  .findpw-page > .auth-box {
+    margin-top: 16px;
+  }
+  .findpw-page > .error {
+    margin-top: 16px;
+    text-align: left;
+    color: red;
+    word-break: keep-all;
+  }
+  .findpw-page > footer {
+    text-align: right;
+    padding: 16px 0;
+  }
+  .findpw-page > footer .button {
+    margin-left: 16px;
+  }
 </style>
--- src/views/user/FindPwPage2.vue
+++ src/views/user/FindPwPage2.vue
@@ -1,15 +1,125 @@
 <template>
-  <div>
-    
-  </div>
+  <form class="findpw-page" @submit.prevent="submit" method="POST">
+    <h1>비밀번호 변경</h1>
+    <p class="message">
+      안전한 사용을 위하여, 기존 비밀번호를 변경해야 합니다.<br>
+      아래에 새 비밀번호를 입력해 주세요.
+    </p>
+    <div class="pw-box">
+      <input-box id="findpw-pw" type="password" placeholder="새 비밀번호"
+        @input="v=>{pw.val=v}" autocomplete="off"/>
+    </div>
+    <div class="pwconfirm-box">
+      <input-box id="findpw-pwconfirm" type="password" placeholder="비밀번호 확인"
+        @input="v=>{confirmPw.val=v}" autocomplete="off"/>
+    </div>
+    <p class="error">{{error}}</p>
+    <footer>
+      <default-button class="button" type="submit" value="변경"/>
+    </footer>
+  </form>
 </template>
 
 <script>
+import DefaultButton from '@/components/buttons/DefaultButton.vue'
+import InputBox from '@/components/form/InputBox.vue'
+
 export default{
-  
+  name: 'user.findpw.page2',
+  components: {
+    DefaultButton,
+    InputBox
+  },
+  props: {
+    auth: {
+      type: String,
+      default: ''
+    },
+    email: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      pw: {
+        val: '',
+        pattern: '^(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*?[!-/:-@\\[-`{-~]).{8,20}$',
+        constraint: '영문, 숫자와 특수문자를 모두 포함하며, 8 ~ 20자 이내여야 합니다.'
+      },
+      confirmPw: {
+        val: ''
+      },
+      error: ''
+    }
+  },
+  methods: {
+    async submit() {
+      if(this.pw.val.length !== 0) {
+        if(this._isValid(this.pw)) {
+          if(this.pw.val === this.confirmPw.val) {
+            const payload = {email: this.email, pw: this.pw.val}
+            try {
+              await this.$api("PUT", `/pw/${this.auth}`, payload)
+              this.$processMessenger('비밀번호가 정상적으로 변경되었습니다.',
+                '/login')
+            } catch(error) {
+              this.error = this.pw.constraint
+            }
+          } else {
+            this.error = '비밀번호가 일치하지 않습니다.'
+          }
+        } else {
+          this.error = this.pw.constraint
+        }
+      } else {
+        this.error = '새 비밀번호를 입력하세요.'
+      }
+    },
+    // Private
+    _isValid(target) {
+      return new RegExp(target.pattern).test(target.val)
+    },
+  },
+  created() {
+    // if(this.auth === '' || this.email === '') {
+    //   this.$handleWrongAccess('/')
+    // } else {
+    //   this.findId()
+    // }
+  }
 }
 </script>
 
 <style scoped>
-
+  .findpw-page {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    width: 300px;
+  }
+  .findpw-page > .message {
+    text-align: left;
+    padding-left: 8px;
+    margin-bottom: 16px;
+    word-break: keep-all;
+  }
+  .findpw-page > .pw-box {
+    margin-bottom: 16px;
+  }
+  .findpw-page > .pwconfirm-box {
+    margin-bottom: 16px;
+  }
+  .findpw-page > .error {
+    margin-bottom: 16px;
+    word-break: keep-all;
+    color: red;
+  }
+  .findpw-page > footer {
+    text-align: right;
+    padding: 16px 0;
+  }
+  .findpw-page > footer > .button {
+    margin-left: 16px;
+  }
 </style>
