c41c3b91b76866f971d9ee3be54a914c7c8a3175
hm4725 <4725hmc@gmail.com>
2022-02-18 16:31:37 +0000

    Reconstruct image component - loading - ratio (old browser support)

--- src/components/utils/ImgComponent.vue
+++ src/components/utils/ImgComponent.vue
@@ -1,5 +1,9 @@
 <template>
-  <img :style="{aspectRatio: ratio}" :src="imgSrc" :alt="title" :title="title" ref="image"/>
+  <div class="img-component">
+    <div v-if="ratio" class="img" :class="{loading: loading.process}"
+      :style="style" :title="title"></div>
+    <img v-else :src="imgSrc" :title="title" :class="{loading: loading.process}"/>
+  </div>
 </template>
 
 <script>
@@ -16,12 +20,20 @@ export default{
     },
     ratio: {
       type: String,
-      default: 'unset'
+      default: ''
     }  
   },
   data() {
     return {
-      imgSrc: require('@/assets/svg/blank-3x4.svg')
+      imgSrc: '',
+      style: {
+        backgroundImage: ``,
+        paddingTop: ''
+      },
+      loading: {
+        src: require('@/assets/svg/loading.svg'),
+        process: false
+      }
     }
   },
   watch: {
@@ -35,11 +47,14 @@ export default{
   methods: {
     lazyLoading(src) {
       if(src) {
-        this.imgSrc = require('@/assets/svg/blank-3x4.svg')
-        let image = new Image()
+        this.loading.process = true
+        this._setSrc(this.loading.src)
+
+        const image = new Image()
         image.src = src
         image.onload = () => {
-          this.imgSrc = src
+          this.loading.process = false
+          this._setSrc(image.src)
           this.$emit("mount")
         }
         image.onerror = () => {
@@ -47,21 +62,61 @@ export default{
             console.error(`NOT FOUND: ${src}`)
             this.$emit("error")
           }
-          this.imgSrc = require('@/assets/svg/blank-3x4.svg')
+          this.loading.process = false
+          this._unsetSrc()
         }
       } else {
-        this.imgSrc = require('@/assets/svg/blank-3x4.svg')
+        this.loading.process = false
+        this._unsetSrc()
         this.$emit("unmount")
+      }
+    },
+    _setSrc(src) {
+      if(this.ratio) {
+        this.style.backgroundImage = `url(${src})`
+      } else {
+        this.imgSrc = src
+      }
+    },
+    _unsetSrc() {
+      if(this.ratio) {
+        this.style.backgroundImage = ''
+      } else {
+        this.imgSrc = ''
       }
     }
   },
+  created() {
+    if(this.ratio) {
+      const aspectRatio = this.ratio.split('/')
+      const width = parseInt(aspectRatio[0])
+      const height = parseInt(aspectRatio[1])
+      const heightRatio = 100 / width * height
+      this.style.paddingTop = isFinite(heightRatio) ?
+        `${heightRatio}%` : '100%'
+    }
+  }
 }
 </script>
 
 <style scoped>
-img {
-  display: block;
-  width: 100%;
-  object-fit: cover;
-}
+  .img-component {
+    width: 100%;
+    text-align: center;
+  }
+  .img-component > div.img{
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .img-component > div.img.loading {
+    background-size: initial;
+  }
+  img {
+    display: inline-block;
+    width: 100%;
+  }
+  img.loading {
+    width: initial;
+  }
 </style>
